092230ca0b94307c81d40ee33920be3a24faabcd
Denis Ploskonenko <ploskonenko@live.ru>
2021-08-13 15:33:37 +0600

    add file

---
+++ task_1.py
@@ -0,0 +1,8 @@
+# Функция принимающая 1 аргмент - год и возвращающая True, если год високосный, и Falce иначе
+
+def is_year_leap(year):
+    if year % 4 == 0:
+        return 'True'
+    return 'False'
+
+print(is_year_leap(int(input('Введите год: '))))
